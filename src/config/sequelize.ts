0aa16a937dffb3d0d3f47bdfed029fb88a896742
Mark <iammkb2002@gmail.com>
2025-01-22 13:03:16 +0800

    fix: test fix vercel

--- src/config/sequelize.ts
+++ src/config/sequelize.ts
@@ -1,9 +1,8 @@
 import { Sequelize } from 'sequelize';
-import pg from 'pg';
 import config from './sequelize-config';
 
 const sequelize = new Sequelize({
-  dialect: pg,
+  dialect: config.dialect,
   host: config.host,
   username: config.username,
   password: config.password,
