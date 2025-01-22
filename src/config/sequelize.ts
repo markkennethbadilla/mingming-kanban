da9e796d7f9a75ed84a20b48a776adbe0ec0abea
Mark <iammkb2002@gmail.com>
2025-01-22 16:31:38 +0800

    fix: supabase uses ipv6 which isnt supported by vercel, switching to postgres via scalingo

--- src/config/sequelize.ts
+++ src/config/sequelize.ts
@@ -3,6 +3,14 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+// Load the TLS certificate if available
+const sslOptions = process.env.DB_SSL
+  ? {
+      require: true,
+      ca: process.env.DB_SSL_CERT,
+    }
+  : undefined;
+
 const sequelize = new Sequelize(
   process.env.DB_DATABASE as string,
   process.env.DB_USERNAME as string,
@@ -12,6 +20,9 @@ const sequelize = new Sequelize(
     port: Number(process.env.DB_PORT),
     dialect: 'postgres',
     dialectModule: (await import('pg')).default,
+    dialectOptions: {
+      ssl: sslOptions,
+    },
   }
 );
 
