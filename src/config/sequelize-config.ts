92bbd246b403710ea98ecf0f581ae7efd1a02c31
Mark <iammkb2002@gmail.com>
2025-01-22 11:13:44 +0800

    refactor: included migration files to github

--- src/config/sequelize-config.ts
+++ src/config/sequelize-config.ts
@@ -19,34 +19,12 @@ interface SequelizeConfig {
   dialect: Dialect;
 }
 
-interface Config {
-  development: SequelizeConfig;
-  test: SequelizeConfig;
-  production: SequelizeConfig;
-}
-
-const config: Config = {
-  development: {
-    username: getEnvVar('DB_USERNAME'),
-    password: getEnvVar('DB_PASSWORD', true), // Allow empty password
-    database: getEnvVar('DB_DATABASE'),
-    host: getEnvVar('DB_HOST'),
-    dialect: getEnvVar('DB_DIALECT') as Dialect,
-  },
-  test: {
-    username: getEnvVar('DB_USERNAME'),
-    password: getEnvVar('DB_PASSWORD', true), // Allow empty password
-    database: getEnvVar('DB_TEST_DATABASE'),
-    host: getEnvVar('DB_HOST'),
-    dialect: getEnvVar('DB_DIALECT') as Dialect,
-  },
-  production: {
-    username: getEnvVar('DB_USERNAME'),
-    password: getEnvVar('DB_PASSWORD', true), // Allow empty password
-    database: getEnvVar('DB_PROD_DATABASE'),
-    host: getEnvVar('DB_HOST'),
-    dialect: getEnvVar('DB_DIALECT') as Dialect,
-  },
+const config: SequelizeConfig = {
+  username: getEnvVar('DB_USERNAME'),
+  password: getEnvVar('DB_PASSWORD', true), // Allow empty password
+  database: getEnvVar('DB_DATABASE'),
+  host: getEnvVar('DB_HOST'),
+  dialect: getEnvVar('DB_DIALECT') as Dialect,
 };
 
 export default config;
--- src/config/sequelize.ts
+++ src/config/sequelize.ts
@@ -1,16 +1,12 @@
 import { Sequelize } from 'sequelize';
-import { Dialect } from 'sequelize';
 import config from './sequelize-config';
 
-const env = process.env.NODE_ENV || 'development';
-const dbConfig = config[env];
-
 const sequelize = new Sequelize({
-  dialect: dbConfig.dialect as Dialect,
-  host: dbConfig.host,
-  username: dbConfig.username,
-  password: dbConfig.password,
-  database: dbConfig.database,
+  dialect: config.dialect,
+  host: config.host,
+  username: config.username,
+  password: config.password,
+  database: config.database,
 });
 
 export default sequelize;
