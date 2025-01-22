cb949e7bfc0483ffe8a42ab1a1ef779de05d4952
Mark <iammkb2002@gmail.com>
2025-01-22 09:39:28 +0800

    fix: task.ts in uppercase at origin

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,6 +1,6 @@
 import sequelize from '../config/sequelize'; // Sequelize instance
 import User from '@/models/user';
-import Task from '@/models/task';
+import Task from '@/models/asdfasdfasdf';
 
 export function initializeModels() {
   User.initModel(sequelize);
