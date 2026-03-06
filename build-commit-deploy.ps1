796057fa7fb493f02e9576ed8536b681291cdc2e
Mark <markkennethbadilla@gmail.com>
2026-03-06 09:40:01 +0800

    feat: consistent pixel cat icons everywhere

--- build-commit-deploy.ps1
+++ build-commit-deploy.ps1
@@ -1,5 +1,5 @@
-# deploy.ps1 - Deploy mingming-kanban to VPS
-# Usage: .\deploy.ps1 [-SkipPush] [-SkipBuild]
+# build-commit-deploy.ps1 - Deploy mingming-kanban to VPS
+# Usage: .\build-commit-deploy.ps1 [-CommitMessage "msg"] [-SkipCommit] [-SkipPush] [-SkipBuild]
 & E:\shared\scripts\deploy-vps.ps1 `
     -VpsDir "/opt/mingming-kanban" `
     -Service "mingming-kanban" `
